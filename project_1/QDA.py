b41585e6010e19d01db8434743de7d189504e16f
Matthias Gültig <matthias2906@t-online.de>
2023-04-05 10:29:46 +0200

    add different data distributions and models

--- project_1/QDA.py
+++ project_1/QDA.py
@@ -1,8 +1,8 @@
-# Code for performing and visualizing knn
+# Code for performing and visualizing QDA
 
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.model_selection import cross_val_score
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -22,31 +22,26 @@ for key in [1,2,3,4]:
                                                         shuffle=True)
 
     # create model
-    # two choieces for knn: metric, number of neighbours k
-    # basic assumptions: k=3, metrix minkowsky -> euclidian metric for p = 2
-
-    # initialize the model
-    n_neighbours=3
-    neigh = KNeighborsClassifier(n_neighbors=n_neighbours, metric="minkowski")
+    clf = QuadraticDiscriminantAnalysis()
 
     # cross validation score
     # train model and calculate cross validation score 
     # number of folds: 10
     # score is the accuracy
-    scores = cross_val_score(neigh, X_train, y_train, cv = 10)
+    scores = cross_val_score(clf, X_train, y_train, cv = 10)
     print(scores)
 
     # train model
-    neigh.fit(X_train, y_train)
+    clf.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, neigh.predict(X_test))
+    acc = accuracy_score(y_test, clf.predict(X_test))
     # create "test" meshgrid
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, .1), np.arange(x2_min, x2_max, .1))
 
     # predict class using "test" meshgrid
-    y = neigh.predict(np.c_[xx1.ravel(), xx2.ravel()])
+    y = clf.predict(np.c_[xx1.ravel(), xx2.ravel()])
     y = y.reshape(xx1.shape)
 
     # plot results of "test" meshgrid
@@ -66,4 +61,4 @@ for key in [1,2,3,4]:
 
     plt.title(f"Tested accuracy: {np.round(acc, 2)}, \n cross val scores: {np.round(scores,2)}")
     # store image
-    plt.savefig(f"knn_{key}")
+    plt.savefig(f"QDA_{key}")
--- project_1/data_gen.py
+++ project_1/data_gen.py
@@ -2,20 +2,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 3 methods for classification task: KNN, DLA, logistic regression.
+# 3 methods for classification task: KNN (local rule), QDA (global rule), logistic regression (global rule).
 # Assume binary classification task with p=2 (2 features x_1 and x_2).
 
+# 4 Assumptions for dataset distributions
+# 1. Small dataset with less overlap (1)
+# 2. Small dataset with large overlap (2) (simple boundary)
+# 3. Large dataset with less overlap (3)
+# 4. Large dataset with large overlap (4) (simple boundary)
+
+# keys: data distribution (1, 2, 3 or 4)
+# values: n_samples, means, covariance matrix
+meta_data = {
+    1: [30, [[0,0], [1,1]], [[[1, 0.5], [0.5, 1]], [[1, -0.5], [-0.5, 1]]]],
+    2: [30, [[0,0], [2,2]], [[[1, 0], [0, 1]], [[1, 0.3], [0.3, 1]]]],
+    3: [1000, [[0,0], [1,1]], [[[1, 0.5], [0.5, 1]], [[1, -0.5], [-0.5, 1]]]],
+    4: [1000, [[0,0], [2,2]], [[[1, 0.5], [0.5, 1]], [[1, -0.5], [-0.5, 1]]]],
+}
+
+# select key and values
+# TODO: Change key here (1,2,3,4)
+key = 4
+data_distrib_info = meta_data[key]
+
 # Set the number of samples per class (Assumption: balanced data sets)
-n_samples = 1000
+n_samples = data_distrib_info[0]
 
 # Define the mean and covariance for each class
-means = np.array([[0, 0], [3, 3]])
+means = np.array(data_distrib_info[1])
 # Intuitive understanding:
 # Main diagonal: variances of x_1 and x_2 (scaling of "original" variances 1)
 # Entries on the side: covariances of x_1 and x_2 (transform them actually into
 # "changed shape")
 # First entry: class 0, second entry: class 1
-covs = np.array([[[1, 0.5], [0.5, 1]], [[1, -0.5], [-0.5, 1]]])
+covs = np.array(data_distrib_info[2])
 
 # Generate the data for each class
 # class 0
@@ -29,11 +49,11 @@ y1 = np.ones(n_samples)
 X = np.concatenate((X0, X1), axis=0)
 y = np.concatenate((y0, y1), axis=0)
 
-np.save("X", X)
-np.save("y", y)
+np.save(f"X_{key}", X)
+np.save(f"y_{key}", y)
 
 # Plot the data
-plt.scatter(X[:,0], X[:,1], c=y)
+plt.scatter(X[:,0], X[:,1], c=y, alpha=0.4)
 plt.xlabel('x1')
 plt.ylabel('x2')
-plt.savefig("data")
+plt.savefig(f"data_{key}")
